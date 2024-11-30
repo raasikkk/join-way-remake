a9cc57ab7fbe90e6dfa5ab06355e0363f48dfe22
zhankeldiulyrasultop1@gmail.com <zhankeldiulyrasultop1@gmail.com>
2024-11-30 23:35:22 +0500

    button size, added container to swiper

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -12,7 +12,10 @@ const Portfolio = () => {
 
   return (
     <>
-      <section id="portfolio" className="portfolio mt-[130px] ">
+      <section
+        id="portfolio"
+        className="portfolio mt-[130px] container mx-auto"
+      >
         <div className="container mx-auto">
           <h1 className="xs:text-[25.34px] sm:text-[28.34px] md:text-[35.34px] lg:text-[42.34px] font-bold text-j-blue dark:text-j-yellow hiddenYEl max-w-[527px]">
             Посмотрите Наше Портфолио
