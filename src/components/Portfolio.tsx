300c4fe0ba47ab657c5da1d7403db4fda0ca9b85
zhankeldiulyrasultop1@gmail.com <zhankeldiulyrasultop1@gmail.com>
2024-12-01 13:57:13 +0500

    user controlled swiper

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -3,29 +3,23 @@ import { clients } from "../constants/clients";
 
 const Portfolio = () => {
   return (
-    <>
-      <section
-        id="portfolio"
-        className="portfolio mt-[130px] container mx-auto overflow-hidden"
-      >
-        <div className="container mx-auto">
-          <h1 className="xs:text-[25.34px] sm:text-[28.34px] md:text-[35.34px] lg:text-[42.34px] font-bold text-j-blue dark:text-j-yellow hiddenYEl max-w-[527px]">
-            Посмотрите Наше Портфолио
-          </h1>
-        </div>
-        {/* <!-- Portfolio Cards --> */}
-        <div className="overflow-hidden mt-24 w-full h-full relative">
-          <div className="flex gap-16 animate-scroll min-h-[600px]">
-            {clients.map((client) => (
-              <Project key={client.id} client={client} />
-            ))}
-            {clients.map((client) => (
-              <Project key={client.id} client={client} />
-            ))}
-          </div>
-        </div>
-      </section>
-    </>
+    <section
+      id="portfolio"
+      className="portfolio mt-[130px] container mx-auto overflow-hidden"
+    >
+      <div className="container mx-auto">
+        <h1 className="xs:text-[25.34px] sm:text-[28.34px] md:text-[35.34px] lg:text-[42.34px] font-bold text-j-blue dark:text-j-yellow hiddenYEl max-w-[527px]">
+          Посмотрите Наше Портфолио
+        </h1>
+      </div>
+
+      {/* Manual Scroll Portfolio */}
+      <div className="mt-24 w-full min-h-[600px] overflow-x-auto flex gap-8 scroll-smooth no-scrollbar">
+        {clients.map((client) => (
+          <Project key={client.id} client={client} />
+        ))}
+      </div>
+    </section>
   );
 };
 
