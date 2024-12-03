760c11d7cc66382add06eb33d57ff139594c2916
zhankeldiulyrasultop1@gmail.com <zhankeldiulyrasultop1@gmail.com>
2024-12-03 09:38:04 +0500

    minor changes

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,11 +1,11 @@
-import axios from "axios";
+// import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/", // Update if using a different base URL
-  headers: {
-    "Content-Type": "application/json",
-  },
-  withCredentials: true, // Ensure cookies are sent if needed
-});
+// const axiosInstance = axios.create({
+//   baseURL: "http://127.0.0.1:8000/api/", // Update if using a different base URL
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   withCredentials: true, // Ensure cookies are sent if needed
+// //
 
-export default axiosInstance;
+// export default axiosInstance;
--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Proxy the /admin path to Django
-      "/admin": {
-        target: "http://localhost:8000", // Django server URL
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/admin/, "/admin"),
-      },
-      // Proxy the /static path to Django static files
-      "/static": {
-        target: "http://localhost:8000", // Django static files server
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
